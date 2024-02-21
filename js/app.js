04f84481cb3d647221913836b18cf1ab1212b6cb
Akinwande Oladipo <54485470+Akinwande-Mhayormarkdee-Oladipo@users.noreply.github.com>
2024-02-21 06:40:34 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,18 +3,26 @@ var i;
 var len= acc.length;    
 
 for (i=0; i<len; i++) {
-    
-    acc[i].addEventListener("click", function(){
-        this.classList.toggle("active");
-        var answer = this.nextElementSibling;
-        // var currentAnswer = document.querySelector(".answer.active");
-        if (answer.style.maxHeight) {
-            answer.style.maxHeight = null;  
-        } else {   
-        
-            answer.style.maxHeight = answer.scrollHeight + "px";
-            
-        }                    
-        
-    })
+
+    acc[i].addEventListener("click", var acc = document.getElementsByClassName("question");
+var i;
+var len = acc.length;
+
+for (i = 0; i < len; i++) {
+  
+    var currentAnswer = document.querySelector(".question.active");
+      if (currentAnswer) {
+        currentAnswer.classList.remove("active");
+      }
+      this.classList.toggle("active");
+    acc[i].addEventListener("click", function () {
+    var answer = this.nextElementSibling;
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
+})
 }
